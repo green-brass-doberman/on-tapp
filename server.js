f0c12588d33a8e45af5bdf643ffb6fbd1fc47d29
Victor Leung <victorleungtw@gmail.com>
2014-12-20 08:46:03 +0800

    add logo

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+// Serve static files
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
+// Get the port from environment variables
+var port = process.env.PORT || 3000;
+
 // Start the app by listening on <port>
-app.listen(3000);
+app.listen(port);
+
+console.log('Server running on port %d', port);
