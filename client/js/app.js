e304b7f777d62d1678ba1ace945f53b30ebcc64c
Victor Leung <victorleungtw@gmail.com>
2014-12-24 00:16:18 +0800

    add routing

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,49 @@
  // create the module and name it onTappApp
-var onTappApp = angular.module('onTappApp', []);
+   // also include ngRoute for all our routing needs
+var onTappApp = angular.module('onTappApp', ['ngRoute']);
+
+// configure our routes
+onTappApp.config(function($routeProvider) {
+$routeProvider
+
+    // route for the home page
+    .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+    })
+
+    // route for the nearby page
+    .when('/nearby', {
+        templateUrl : 'pages/nearby.html',
+        controller  : 'nearByController'
+    })
+
+    // route for the contact page
+    .when('/ratings', {
+        templateUrl : 'pages/ratings.html',
+        controller  : 'ratingsController'
+    });
+});
+
 
 // create the controller and inject Angular's $scope
 onTappApp.controller('mainController', function($scope) {
-  $scope.name = "user";
+  $scope.name = 'user';
   $scope.sayHello = function() {
-    $scope.greeting = "Hello " + $scope.name;
+    $scope.greeting = 'Hello ' + $scope.name;
+  };
+});
+
+onTappApp.controller('nearByController', function($scope) {
+  $scope.name = 'user';
+  $scope.sayHello = function() {
+    $scope.greeting = 'Hello ' + $scope.name;
+  };
+});
+
+onTappApp.controller('ratingsController', function($scope) {
+  $scope.name = 'user';
+  $scope.sayHello = function() {
+    $scope.greeting = 'Hello ' + $scope.name;
   };
 });
