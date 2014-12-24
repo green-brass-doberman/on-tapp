c8a0026da79e0751bd11b424be4401e0a24b9822
Victor Leung <victorleungtw@gmail.com>
2014-12-24 12:46:32 +0800

    ng-repeat for nearby brewery data

--- client/js/app.js
+++ client/js/app.js
@@ -4,69 +4,64 @@ var onTappApp = angular.module('onTappApp', ['ngRoute']);
 
 // configure our routes
 onTappApp.config(function($routeProvider) {
-$routeProvider
-  // route for the home page
-  .when('/', {
-    templateUrl : 'pages/home.html',
-    controller  : 'MainController'
-  })
+  $routeProvider
+    // route for the home page
+    .when('/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'MainController'
+    })
 
-  // route for the nearby page
-  .when('/nearby', {
-    templateUrl : 'pages/nearby.html',
-    controller  : 'NearByController'
-  })
+    // route for the nearby page
+    .when('/nearby', {
+      templateUrl : 'pages/nearby.html',
+      controller  : 'NearByController'
+    })
 
-  // route for the contact page
-  .when('/ratings', {
-    templateUrl : 'pages/ratings.html',
-    controller  : 'RatingsController'
-  })
+    // route for the contact page
+    .when('/ratings', {
+      templateUrl : 'pages/ratings.html',
+      controller  : 'RatingsController'
+    })
 
-  // route for authetications
-  .when('/signup', {
-    templateUrl: 'pages/signup.html',
-    controller: 'AuthController',
-  })
+    // route for authetications
+    .when('/signup', {
+      templateUrl: 'pages/signup.html',
+      controller: 'AuthController',
+    })
 
-  .when('/signin', {
-    templateUrl: 'pages/signin.html',
-    controller: 'AuthController'
-  })
+    .when('/signin', {
+      templateUrl: 'pages/signin.html',
+      controller: 'AuthController'
+    })
 
-  .otherwise({
-    redirectTo: '/'
-  });
+    .otherwise({
+      redirectTo: '/'
+    });
 });
 
 
 // create the controller and inject Angular's $scope
-onTappApp.controller('MainController', function($scope) {
+onTappApp.controller('MainController', ['$scope', function($scope) {
   $scope.name = 'user';
   $scope.sayHello = function() {
     $scope.greeting = 'Hello ' + $scope.name;
   };
-});
+}]);
 
-onTappApp.controller('NearByController', function($scope) {
-  $scope.name = 'user';
-  $scope.sayHello = function() {
-    $scope.greeting = 'Hello ' + $scope.name;
-  };
-});
+onTappApp.controller('NearByController', ['$scope', function($scope) {
+  $scope.breweries = [
+    {name:'XXXX Brewery', description: 'Founded in 2015, it has the best beer in the world.', distance: 100},
+    {name:'YYYY Brewery', description: 'Founded in 1915, it has the worst beer in the world.', distance: 200},
+    {name:'ZZZZ Brewery', description: 'Founded in 1815, it no longer brews any beer for the world.',  distance: 300}
+  ];
+}]);
 
-onTappApp.controller('RatingsController', function($scope) {
+onTappApp.controller('RatingsController', ['$scope', function($scope) {
   $scope.name = 'user';
-  $scope.sayHello = function() {
-    $scope.greeting = 'Hello ' + $scope.name;
-  };
-});
+}]);
 
-onTappApp.controller('AuthController', function($scope) {
+onTappApp.controller('AuthController', ['$scope', function($scope) {
   $scope.name = 'user';
-  $scope.sayHello = function() {
-    $scope.greeting = 'Hello ' + $scope.name;
-  };
-});
+}]);
 
 
