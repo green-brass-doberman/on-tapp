b13aa39560f6d081cbbb5459cb85286fcf27e354
Victor Leung <victorleungtw@gmail.com>
2014-12-27 02:27:21 +0800

    set up proxy server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var proxy = require('simple-http-proxy');
+var keys = require('./../api-key');
 var app = express();
+
+opts = {
+  proto: 'x-orig-proto',
+  host: 'x-orig-host',
+  port: 'x-orig-port',
+  path: 'x-orig-path'
+};
 
 // Serve static files
 app.use('/', express.static(path.join(__dirname, '../client')));
+
+app.use('/api', proxy('http://api.brewerydb.com/v2/?key=' + keys.brewerydb, opts));
+
 app.use(bodyParser.json());
 
 // export our app for testing and flexibility, required by index.js
