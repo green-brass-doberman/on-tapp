a544ba23a3f8f070d01a9ca4d5dbd7aa725d68bc
Victor Leung <victorleungtw@gmail.com>
2014-12-26 21:48:32 +0800

    add maker on google map

--- client/js/nearby.js
+++ client/js/nearby.js
@@ -12,4 +12,16 @@ angular.module('onTappApp.nearby', [])
 
     // render Google map and set center at San Francisco by default
     $scope.map = { center: { latitude: 37.7833, longitude: -122.4167 }, zoom: 8 };
+
+    $scope.options = {scrollwheel: false};
+
+    $scope.marker = {
+      id: 0,
+      coords: {
+        latitude: 37.7833,
+        longitude: -122.4167
+      },
+      options: { draggable: false }
+    };
+
   }]);
