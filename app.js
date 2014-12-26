3190c0f21cd74bc922ffa314f5a86c220276dc70
JulieMarie <julie@clearlycoded.com>
2014-12-26 15:27:59 -0800

    deleted app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var app = require('./server/server.js');
-
-// Get the port from environment variables
-var port = process.env.PORT || 3000;
-// Start the app by listening on <port>
-app.listen(port);
-
-console.log('Server running on port %d', port);
