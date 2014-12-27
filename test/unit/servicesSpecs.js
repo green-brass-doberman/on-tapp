b70e402fdc82eb4a8c57ee1b6b02d991449af41b
Victor Leung <victorleungtw@gmail.com>
2014-12-27 22:58:42 +0800

    fix karma test

--- test/unit/servicesSpecs.js
+++ test/unit/servicesSpecs.js
@@ -10,10 +10,6 @@ describe('Unit: Services', function () {
   });
 
   it('Should instantiate with 3 breweries', function () {
-    expect(factory.breweries).to.eql([
-        {name:'XXXX Brewery', description: 'Founded in 2015, it has the best beer in the world.', distance: 100, ratings: 0},
-        {name:'YYYY Brewery', description: 'Founded in 1915, it has the worst beer in the world.', distance: 200, ratings: 0},
-        {name:'ZZZZ Brewery', description: 'Founded in 1815, it no longer brews any beer for the world.',  distance: 300, ratings: 0}
-      ]);
+    expect(factory.getData).to.be.a('function');
   });
 });
