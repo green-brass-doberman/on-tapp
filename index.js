de26498ed21f9b9069cc5501a8fc2e66a1d58431
Victor Leung <victorleungtw@gmail.com>
2014-12-27 22:30:43 +0800

    change proxy server to request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('./server/server.js');
 
 // Get the port from environment variables
 var port = process.env.PORT || 3000;
+
 // Start the app by listening on <port>
 app.listen(port);
 
--- server/server.js
+++ server/server.js
@@ -1,23 +1,40 @@
+// set up
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
-var proxy = require('simple-http-proxy');
 var secret = require('./../api-key');
+var request = require('request');
+
+// create our app w/ express
 var app = express();
 
-opts = {
-  proto: 'x-orig-proto',
-  host: 'x-orig-host',
-  port: 'x-orig-port',
-  path: 'x-orig-path'
-};
+// create our app w/ express
+var mongoose = require('mongoose');
 
-// Serve static files
+// log requests to the console (express4)
+var morgan = require('morgan');
+
+// pull information from HTML POST (express4)
+var bodyParser = require('body-parser');
+
+// set the static files location /client for users
 app.use('/', express.static(path.join(__dirname, '../client')));
 
-app.use('/api', proxy('http://api.brewerydb.com/v2/?key=' + secret.keys.brewerydb, opts));
+// log every request to the console
+app.use(morgan('dev'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({'extended':'true'}));
+
+// parse application/json
 app.use(bodyParser.json());
+
+app.use('/api', function(){
+  request('http://api.brewerydb.com/v2/?key='+secret.keys.brewerydb, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Show the HTML for the Google homepage.
+    }
+  });
+});
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
