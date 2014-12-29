baa9236dd3c4d352f4ab2c4d1ad1803312afbdba
Victor Leung <victorleungtw@gmail.com>
2014-12-30 03:47:37 +0800

    use meanjs boilerplater

--- karma.conf.js
+++ karma.conf.js
@@ -1,88 +1,51 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var applicationConfiguration = require('./config/config');
+
 // Karma configuration
-// Generated on Sat Dec 20 2014 07:27:54 GMT+0800 (HKT)
-
 module.exports = function(config) {
-  config.set({
+	config.set({
+		// Frameworks to use
+		frameworks: ['jasmine'],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+		// List of files / patterns to load in the browser
+		files: applicationConfiguration.assets.lib.js.concat(applicationConfiguration.assets.js, applicationConfiguration.assets.tests),
 
+		// Test results reporter to use
+		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+		reporters: ['progress'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon'],
+		// Web server port
+		port: 9876,
 
+		// Enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-    // list of files / patterns to load in the browser
-    files: require('./include.conf.js').concat([
-      'test/unit/**/*.js',
-      'test/integration/**/*.js'
-    ]),
+		// Level of logging
+		// Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
+		// Enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
-    // list of files to exclude
-    exclude: [
-      'karma.conf.js'
-    ],
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['PhantomJS'],
 
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 60000,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/client/js/**/*.js': ['coverage']
-    },
-
-    // Configure the reporter
-    coverageReporter: {
-      type: 'html',
-      dir: 'results/coverage/'
-    },
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 20000,
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress',  'coverage'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-     // report which specs run too slow
-    reportSlowerThan: 500,
-
-    // any additional plugins needed for testing
-    plugins: [
-      'karma-coverage',
-      'karma-mocha',
-      'karma-chai',
-      'karma-sinon',
-      'karma-chrome-launcher'
-    ]
-  });
+		// Continuous Integration mode
+		// If true, it capture browsers, run tests and exit
+		singleRun: true
+	});
 };
