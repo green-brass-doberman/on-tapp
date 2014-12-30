6c06d1bfe17e09e4410bd14ad6c9fb1eaf9cba60
Victor Leung <victorleungtw@gmail.com>
2014-12-30 09:08:09 +0800

    fix karma test dependency

--- config/env/test.js
+++ config/env/test.js
@@ -19,7 +19,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Test Environment'
+		title: 'OnTapp - Test Environment'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
