21a0fe6b3d330080362f1fa733222b520484814d
JulieMarie <julie@clearlycoded.com>
2014-12-29 17:24:22 -0800

    Added in FB, Twitter and Google strategies

--- config/env/test.js
+++ config/env/test.js
@@ -22,30 +22,30 @@ module.exports = {
 		title: 'OnTapp - Test Environment'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		clientID: process.env.FACEBOOK_ID || '968149419880980',
+		clientSecret: process.env.FACEBOOK_SECRET || 'fc60727d78e8fcd2e975822c8eef619e',
 		callbackURL: '/auth/facebook/callback'
 	},
 	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		clientID: process.env.TWITTER_KEY || 'k186DeKdHcNN4Vlk8lB9MmjBW',
+		clientSecret: process.env.TWITTER_SECRET || '9V0X34pyjBuixI8bRwuGf639spcOWs91Q1Fb9Iq4qjDPRgsCf7',
 		callbackURL: '/auth/twitter/callback'
 	},
 	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		clientID: process.env.GOOGLE_ID || '768801944338-ms3g3ic84auevq51a2t4cld7ept7ffos.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || '5xxSzR-ltreqF7EloyCt9JVC',
 		callbackURL: '/auth/google/callback'
 	},
-	linkedin: {
-		clientID: process.env.LINKEDIN_ID || 'APP_ID',
-		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-		callbackURL: '/auth/linkedin/callback'
-	},
-	github: {
-		clientID: process.env.GITHUB_ID || 'APP_ID',
-		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
-		callbackURL: '/auth/github/callback'
-	},
+	// linkedin: {
+	// 	clientID: process.env.LINKEDIN_ID || 'APP_ID',
+	// 	clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+	// 	callbackURL: '/auth/linkedin/callback'
+	// },
+	// github: {
+	// 	clientID: process.env.GITHUB_ID || 'APP_ID',
+	// 	clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+	// 	callbackURL: '/auth/github/callback'
+	// },
 	mailer: {
 		from: process.env.MAILER_FROM || 'MAILER_FROM',
 		options: {
