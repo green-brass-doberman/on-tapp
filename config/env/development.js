d34824bc861c2abd75c45efbc01cc94880e23277
JulieMarie <julie@clearlycoded.com>
2014-12-29 18:09:30 -0800

    Password reset working.  Using ontappapp@gmail.com

--- config/env/development.js
+++ config/env/development.js
@@ -46,12 +46,12 @@ module.exports = {
 	// 	callbackURL: '/auth/github/callback'
 	// },
 	mailer: {
-		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		from: process.env.MAILER_FROM || 'ontappapp@gmail.com',
 		options: {
-			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
 			auth: {
-				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+				user: process.env.MAILER_EMAIL_ID || 'ontappapp@gmail.com',
+				pass: process.env.MAILER_PASSWORD || 'Cs0cs5#BVfrBR5a&ZK@Z'
 			}
 		}
 	}
