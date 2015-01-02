5022ac7d97b4ecc3288d121fc1d028164ff475be
Victor Leung <victorleungtw@gmail.com>
2015-01-02 21:30:55 +0800

    remove firebase

--- public/modules/nearby/controllers/nearby.client.controller.js
+++ public/modules/nearby/controllers/nearby.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('nearby').controller('NearbyController', ['$scope', 'Breweries', 'geolocation', '$stateParams', '$firebase', 'uiGmapLogger',
-	function($scope, Breweries, geolocation, $stateParams, $firebase, uiGmapLogger) {
+angular.module('nearby').controller('NearbyController', ['$scope', 'Breweries', 'geolocation', '$stateParams', 'uiGmapLogger',
+	function($scope, Breweries, geolocation, $stateParams, uiGmapLogger) {
 		// Controller Logic
 		// ...
 
@@ -96,47 +96,8 @@ angular.module('nearby').controller('NearbyController', ['$scope', 'Breweries', 
 
     // function to save ratings
     $scope.saveRating = function(){
-      var brewery = allBreweries[0];
-      brewery.ratings = $scope.percent;
-    };
-
-    // connect to firebase
-    var ref = new Firebase('https://on-tapp.firebaseio.com/ratings');
-
-    var fb = $firebase(ref);
-
-    // function to set the default data
-    $scope.reset = function() {
-
-      $scope.rate = 0;
-
-      fb.$set({
-        xxxx: {
-          name: 'XXXX Brewery',
-          ratings: {
-            stars: {
-              number: '0',
-              rated: false
-            }
-          }
-        },
-        yyyy: {
-          name: 'YYYY Brewery',
-          ratings: {
-            stars: {
-              number: '0',
-              rated: false
-            }
-          }
-        }
-      });
 
     };
 
-    // sync as object
-    var syncObject = fb.$asObject();
-
-    // three way data binding
-    syncObject.$bindTo($scope, 'ratings');
   }
 ]);
