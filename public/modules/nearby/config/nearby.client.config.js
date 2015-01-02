0a90607ae486b836cc164cef13659ca96b20c427
Victor Leung <victorleungtw@gmail.com>
2015-01-02 22:33:38 +0800

    add navbar menu the right way

---
+++ public/modules/nearby/config/nearby.client.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('nearby').run(['Menus',
+  function(Menus) {
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', 'Nearby', 'nearby', '/nearby');
+  }
+]);
