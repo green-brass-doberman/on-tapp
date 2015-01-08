61d87b6a1f46fa770f259d68955cc374d4b4a06c
Victor Leung <victorleungtw@gmail.com>
2015-01-09 04:34:59 +0800

    add font-awesome css

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,7 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/fontawesome/css/font-awesome.min.css',
         'public/lib/flat-ui/dist/css/flat-ui.min.css'
 			],
 			js: [
