5d9f8777bec2858e2c715b17c99fa5b092a20bb7
Julie Knowles <JulieMarie@users.noreply.github.com>
2015-01-08 16:28:54 -0800

    Updated custom connection string for mongolabs use in Azure

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+		uri: process.env.MONGOHQ_URL || process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		options: {
 			user: '',
 			pass: ''
