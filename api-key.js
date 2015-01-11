af9afb8d541c3a387089479b3ffbeacbda1aed69
Victor Leung <victorleungtw@gmail.com>
2015-01-12 06:16:53 +0800

    working with localhost recommendation engine

--- api-key.js
+++ api-key.js
@@ -1,4 +1,4 @@
 exports.keys = {
   brewerydb : '87f92c3b32c0d8e031f828b0d03c2c2a',
-  predictionio: 'oa2276vwsfYQa8AkM1a90TH4vkCXkfFCujbzKp7WbzpctXCAWV9lqtkPly2IJMEk'
+  predictionio: 'hmQhiIak1jIyaE4haGDSzUBhTd43XKZxkTfTRcyL9MPueUohvQcVdFnAB0smNQb6'
 };
