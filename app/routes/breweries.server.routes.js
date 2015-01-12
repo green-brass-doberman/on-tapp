dc561a72ce3eda68271a860e959a5fdc2a171fae
Victor Leung <victorleungtw@gmail.com>
2015-01-13 07:42:53 +0800

    change breweries server

--- app/routes/breweries.server.routes.js
+++ app/routes/breweries.server.routes.js
@@ -5,8 +5,8 @@ var request = require('request');
 module.exports = function(app) {
   // lat and lng contain the current location latitude and longitude to allow listing
   // of nearby breweries
-  app.get('/breweries/:lat/:lng/:rad', function(req, res){
-    request('https://api.brewerydb.com/v2/search/geo/point?lat=' + req.params.lat + '&lng=' + req.params.lng + '&radius=' + req.params.rad + '&key=' + secret.keys.brewerydb, function (error, response, body) {
+  app.get('/breweries/:lat/:lng/', function(req, res){
+    request('https://api.brewerydb.com/v2/search/geo/point?lat=' + req.params.lat + '&lng=' + req.params.lng + '&key=' + secret.keys.brewerydb, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         res.send(body);
       }
