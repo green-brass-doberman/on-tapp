c97b1ef5fd7abc200f5aaafb1b77070841eeedfb
Victor Leung <victorleungtw@gmail.com>
2015-01-15 07:21:22 +0800

    fix beer route

---
+++ public/modules/beers/config/beers.client.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Setting up route
+angular.module('beers').config(['$stateProvider',
+  function($stateProvider) {
+    // Beers state routing
+    $stateProvider.
+    state('beer', {
+      url: '/beer/:beerId',
+      templateUrl: 'modules/beers/views/beers.client.view.html'
+    });
+  }
+]);
