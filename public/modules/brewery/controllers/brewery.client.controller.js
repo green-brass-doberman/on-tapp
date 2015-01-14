cc49a7597613014f4bca2d642c1c0994b36c5e96
Victor Leung <victorleungtw@gmail.com>
2015-01-15 03:53:27 +0800

    move list of beers to brewery page

--- public/modules/brewery/controllers/brewery.client.controller.js
+++ public/modules/brewery/controllers/brewery.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('nearby').controller('BreweryController', ['$scope', 'Brewery', '$stateParams',
-  function($scope, Brewery, $stateParams) {
+angular.module('nearby').controller('BreweryController', ['$scope', 'Brewery', '$stateParams', 'Ratings', '$location',
+  function($scope, Brewery, $stateParams, Ratings, $location) {
     // Brewery controller logic
     $scope.breweryId = $stateParams.breweryId;
     var holdSocial = [];
@@ -21,5 +21,101 @@ angular.module('nearby').controller('BreweryController', ['$scope', 'Brewery', '
         $scope.socialMedia = holdSocial;
       }
     });
+
+    // sort the given collection on the given property
+    function sortOn(collection, name) {
+      collection.sort(
+        function(a, b) {
+          if (a[name] <= b[name]) {
+            return(-1);
+          }
+          return(1);
+        }
+      );
+    }
+
+    // group the beers list on the given property
+    $scope.groupBy = function(attribute) {
+      // First, reset the groups.
+      $scope.groups = [];
+
+      // Now, sort the collection of beers on the grouping-property.
+      // This just makes it easier to split the collection.
+      sortOn($scope.beers, attribute);
+
+      // I determine which group we are currently in.
+      var groupValue = '_INVALID_GROUP_VALUE_';
+
+      // As we loop over each beer, add it to the current group -
+      // we'll create a NEW group every time we come across a new attribute value.
+      for (var i = 0; i < $scope.beers.length; i++) {
+        var beer = $scope.beers[i];
+
+        var group;
+
+        // Should we create a new group?
+        if (beer[attribute] !== groupValue) {
+          group = {
+            label: beer[attribute],
+            beers: []
+          };
+          groupValue = group.label;
+          $scope.groups.push(group);
+        }
+
+        // Add the friend to the currently active grouping.
+        group.beers.push(beer);
+      }
+    };
+
+    $scope.beers = [];
+    $scope.groups = [];
+    $scope.breweryId = $stateParams.breweryId;
+
+    var handleSuccess = function(data, status){
+      $scope.beers = data.data;
+      $scope.groupBy('availableId');
+    };
+
+    // send the brewery id
+    Brewery.getBeersData($scope.breweryId).success(handleSuccess);
+
+    // handle the stars rating
+    $scope.rate = 0;
+    $scope.max = 5;
+    $scope.isReadonly = false;
+
+    // hoveing over on ratings stars
+    $scope.hoveringOver = function(value) {
+      $scope.overStar = value;
+      $scope.percent = 100 * (value / $scope.max);
+    };
+
+    // set the ratings stars
+    $scope.ratingStates = [
+      {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+    ];
+
+    // Create new Rating
+    $scope.create = function(index) {
+
+      // Create new Rating object
+      var rating = new Ratings ({
+        beerId: $scope.beers[index].id,
+        name: $scope.beers[index].name,
+        stars: this.rate,
+        styleName: $scope.beers[index].style.name
+      });
+
+      // Redirect after save
+      rating.$save(function(response) {
+        $location.path('ratings/' + response._id);
+
+        // Clear form fields
+        $scope.name = '';
+      }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
   }
 ]);
