6c6c02c47045b39d53187ba569c209604f7d89f6
Victor Leung <victorleungtw@gmail.com>
2015-01-17 07:36:24 +0800

    add details on recommended beer

--- public/modules/beer/controllers/beer.client.controller.js
+++ public/modules/beer/controllers/beer.client.controller.js
@@ -8,9 +8,29 @@ angular.module('beer').controller('BeerController', ['$scope', 'Beer', '$statePa
     // an array to store recommendations
     $scope.recommendations = [];
 
+    var shuffle = function (array) {
+      var currentIndex = array.length, temporaryValue, randomIndex ;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    };
+
     // pushing recommendations data from $http request
     var handleSuccess = function(data, status){
-      $scope.recommendations = data.data;
+      console.log(data.data);
+      $scope.recommendations = shuffle(data.data);
     };
 
     // Find the beers in the same category
